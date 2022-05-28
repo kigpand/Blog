65db0003a85c28dc3039bbed6db7baafc92f836e
kigpand <kigpand@naver.com>
2022-05-28 17:33:07 +0900

    logo click 시 home으로 이동하도록 처리

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,18 @@
 import './Header.scss';
 import '../../styles/utill.scss';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Header = () => {
 
+    const nav = useNavigate();
+
+    function onLogoClick() {
+        nav('/');
+    }
+
     return(
         <div className='header'>
-            <div className="logo">로고</div>
+            <div className="logo" onClick={onLogoClick}>로고</div>
             <div className="header_buttons">
                 <a target="_blank" href='https://github.com/kigpand' rel="noreferrer" className="git">깃허브</a>
                 <a href='https://www.notion.so/c7aff4da8b6b4e9689dd4870723ebd1a' target="_blank" rel="noreferrer" className="notion">노션</a>
