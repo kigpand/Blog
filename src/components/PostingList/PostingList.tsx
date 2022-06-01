a630fb9aad4639e24870f0e1280557264fe121d7
kigpand <kigpand@naver.com>
2022-06-01 16:59:46 +0900

    scroll 안내려가던 이슈 처리

--- src/components/PostingList/PostingList.tsx
+++ src/components/PostingList/PostingList.tsx
@@ -8,6 +8,15 @@ const PostingList = () => {
         { title: '첫번째 타이틀', content: '얄리얄리얄라성 얄랄리 얄라', hashTag: ['123', '456', '789'], date: new Date() },
         { title: '두번째 타이틀', content: '얄리얄리얄라성 얄랄리 얄라', hashTag: ['2222'], date: new Date() },
         { title: '세번째 타이틀', content: '얄리얄리얄라성 얄랄리 얄라', hashTag: ['aas', 'dddd'], date: new Date() },
+        { title: '세번째 타이틀', content: '얄리얄리얄라성 얄랄리 얄라', hashTag: ['aas', 'dddd'], date: new Date() },
+        { title: '세번째 타이틀', content: '얄리얄리얄라성 얄랄리 얄라', hashTag: ['aas', 'dddd'], date: new Date() },
+        { title: '세번째 타이틀', content: '얄리얄리얄라성 얄랄리 얄라', hashTag: ['aas', 'dddd'], date: new Date() },
+        { title: '세번째 타이틀', content: '얄리얄리얄라성 얄랄리 얄라', hashTag: ['aas', 'dddd'], date: new Date() },
+        { title: '세번째 타이틀', content: '얄리얄리얄라성 얄랄리 얄라', hashTag: ['aas', 'dddd'], date: new Date() },
+        { title: '세번째 타이틀', content: '얄리얄리얄라성 얄랄리 얄라', hashTag: ['aas', 'dddd'], date: new Date() },
+        { title: '세번째 타이틀', content: '얄리얄리얄라성 얄랄리 얄라', hashTag: ['aas', 'dddd'], date: new Date() },
+        { title: '세번째 타이틀', content: '얄리얄리얄라성 얄랄리 얄라', hashTag: ['aas', 'dddd'], date: new Date() },
+        { title: '세번째 타이틀', content: '얄리얄리얄라성 얄랄리 얄라', hashTag: ['aas', 'dddd'], date: new Date() },
     ];
 
     return(
